f139e41c97fe07be8f6264e7a5b23b62ea9ab839
ahsanarshad2002 <ahsanarshad291@gmail.com>
2026-05-25 03:44:30 +0500

    new web

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
 import MarqueeText from '@/components/MarqueeText'
@@ -33,56 +32,3 @@ export default function Home() {
     </main>
   )
 }
-=======
-"use client";
-import React, { useEffect, useState } from "react";
-import Introsection from "@/app/Components/Home/Introsection";
-import Aboutsection from "@/app/Components/Home/Aboutsection";
-import SkillsSection from "@/app/Components/Home/SkillsSection";
-import ExperienceSection from "@/app/Components/Home/ExperienceSection";
-import PortfolioSection from "@/app/Components/Home/PortfolioSection";
-import HireMeSection from "@/app/Components/Home/HireMeSection";
-import Navbar from "@/app/Components/Home/Navbar";
-import Footer from "@/app/Components/Home/Footer";
-import Head from "next/head";
-import AOS from "aos";
-import "aos/dist/aos.css";
-
-
-const HomeSection = () => {
-  useEffect(() => {
-    AOS.init({ duration: 1000 });
-  }, []);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate load time, or use actual logic if needed
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000); // 2 seconds delay
-    return () => clearTimeout(timer);
-  }, []);
-
-  return (
-    <>
-
-
-
-
-      <Navbar />
-      <Introsection />
-      <Aboutsection />
-      <SkillsSection />
-      <ExperienceSection />
-      <PortfolioSection />
-      <HireMeSection />
-
-      <Footer />
-
-
-    </>
-  );
-};
-
-export default HomeSection;
->>>>>>> dc18b16f4851121c1b69831c6a1ea34a9010fdc4
